ff11f0c082ae772102e3e1f0c487d5ef42e98c6a
Amr Abdelhalim <amrgomaa2002@outlook.com>
2025-12-12 00:17:11 +0200

    First Commit

---
+++ app/page.js
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <h1>اهلا بك في موقع علمني</h1>
+    </main>
+  )
+}
